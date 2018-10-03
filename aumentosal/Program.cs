d9a5fa75bb24edc38bbf3b0aedf47a941d420910
Luana Clementino de Moura <46863229870@info.local>
2018-10-03 11:26:02 -0300

    Exercícios com switch case!

---
+++ aumentosal/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace aumentosal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Utilizando CASE faça um programa que receba o nome, salário
+            e código correspondente ao cargo do funcionário e imprima o seu nome,
+            salário, código, cargo, Percentual de aumento e salário com aumento de
+            acordo com o percentual da tabela abaixo:
+            Código Cargo Percentual
+            1 Escriturário 50%
+            2 Secretário 35%
+            3 Caixa 20%
+            4 Gerente 10%
+            5 Diretor Não tem aumento */
+
+            string nome;
+            float salario, aumento, nvsalario;
+            int codigo;
+
+            Console.WriteLine ("Digite seu nome!");
+            nome = Console.ReadLine();
+            
+            Console.WriteLine("Digite seu salário!");
+            salario = float.Parse (Console.ReadLine());
+
+            Console.WriteLine ("Digite o código referente ao seu cargo!");
+            Console.WriteLine ("1 - Escrituário");
+            Console.WriteLine ("2 - Secretário");
+            Console.WriteLine ("3 - Caixa");
+            Console.WriteLine ("4 - Gerente");
+            Console.WriteLine ("5 - Diretor");
+            codigo = int.Parse (Console.ReadLine());
+
+            switch (codigo)
+            {
+                case 1: {
+                    aumento = (50 * salario)/100;
+                    nvsalario = salario + aumento;
+                    Console.WriteLine ($"Nome: {nome}");
+                    Console.WriteLine ($"Salario: R${salario}, cargo {codigo} Escrituário!");
+                    Console.WriteLine ("Percentual de aumento: 50%");
+                    Console.WriteLine ($"Salário com aumento: R${nvsalario}!");
+                    break;
+                }
+
+                case 2: {
+                    aumento = (35 * salario)/100;
+                    nvsalario = salario + aumento;
+                    Console.WriteLine ($"Nome: {nome}");
+                    Console.WriteLine ($"Salario: R${salario}, cargo {codigo} Secretário!");
+                    Console.WriteLine ("Percentual de aumento: 35%");
+                    Console.WriteLine ($"Salário com aumento: R${nvsalario}!");
+                    break;
+                }
+
+                case 3: {
+                    aumento = (20 * salario)/100;
+                    nvsalario = salario + aumento;
+                    Console.WriteLine ($"Nome: {nome}");
+                    Console.WriteLine ($"Salario: R${salario}, cargo {codigo} Caixa!");
+                    Console.WriteLine ("Percentual de aumento: 20%");
+                    Console.WriteLine ($"Salário com aumento: R${nvsalario}!"); 
+                    break;
+                }
+
+                case 4: {
+                    aumento = (10 * salario)/100;
+                    nvsalario = salario + aumento;
+                    Console.WriteLine ($"Nome: {nome}");
+                    Console.WriteLine ($"Salario: R${salario}, cargo {codigo} Gerente!");
+                    Console.WriteLine ("Percentual de aumento: 10%");
+                    Console.WriteLine ($"Salário com aumento: R${nvsalario}!");
+                    break;
+                }
+
+                case 5: {
+                    Console.WriteLine ($"Nome: {nome}");
+                    Console.WriteLine ($"Salario: R${salario}, cargo {codigo} Diretor!");
+                    Console.WriteLine ("Percentual de aumento: Não há aumento!");
+                    break;
+                }
+
+                default: {
+                    Console.WriteLine ("Opção inválida!");
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ rodiziocarros/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace rodiziocarros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Faça um programa que a pessoa insira o ultimo digito da placa do carro
+            e mostre qual é o dia de rodízio! */
+            int ultdig;
+            Console.WriteLine("Informe o último dígito da placa o seu carro!");
+            ultdig = int.Parse (Console.ReadLine());
+
+            switch (ultdig)
+            {
+                case 1: case 2: {
+                    Console.WriteLine("Rodízio na segunda-feira!");
+                    break;
+                }
+
+                case 3: case 4: {
+                    Console.WriteLine("Rodízio na terça-feira!");
+                    break;
+                }
+
+                case 5: case 6: {
+                    Console.WriteLine("Rodízio na quarta-feira!");
+                    break;
+                }
+
+                case 7: case 8: {
+                    Console.WriteLine("Rodízio na quinta-feira!");
+                    break;
+                }
+
+                case 9: case 0: {
+                    Console.WriteLine("Rodízio na sexta-feira!");
+                    break;
+                }
+
+                default: {
+                    Console.WriteLine ("Dígito inválido!");
+                    break;
+                }
+            }
+        }
+    }
+}
