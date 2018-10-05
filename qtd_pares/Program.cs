15ff542c23ff1575bddb1458d8a90fc035a5706c
Luana Clementino de Moura <46863229870@info.local>
2018-10-05 10:30:53 -0300

    Exercícios com do-while

---
+++ qtd_pares/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace qtd_pares
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Peça um numero para o usuário e e informe os números pares e a quantidade
+            de numeros pares no intervalo de 0 ate o numero informado pelo usuário! (naõ conta o 0 e o num então) */
+            int num, qtd=0, i=1;
+
+            Console.WriteLine("Insira um número!");
+            num = int.Parse(Console.ReadLine());
+            
+            do {
+                if (i%2==0) {
+                    qtd ++;
+                    Console.WriteLine(i);
+                }
+                i++;
+            }
+            while(i<num);
+
+            Console.WriteLine($"A quantidade de números pares é {qtd}!");
+
+            /*maneira alternativa de resolver: -------------------------------------------------------------------------------
+            sem if e qtd*/
+            
+            Console.WriteLine("Escreva um número!");
+            num = int.Parse(Console.ReadLine());
+            i=2;
+
+            do {
+                Console.WriteLine (i);
+                i +=2;
+            }
+            while(i<num);
+
+            Console.WriteLine($"A quantidade de pares é {(num/2) -1}!"); // -1 pq não pode contar o próprio número!
+        }
+    }
+}
