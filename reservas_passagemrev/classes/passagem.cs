0260a388ad6aef094f9b2fe1ce24a1f8d710c11a
Luana Clementino de Moura <46863229870@info.local>
2018-10-17 10:40:06 -0300

    Exercícios orientados a objeto

---
+++ reservas_passagemrev/classes/passagem.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace reservas_passagemrev.classes
+{
+    public class passagem
+    {
+        public int npassagem {get; set;}
+        public string nome {get; set;}
+        public DateTime datadovoo {get; set;}
+    }
+}
